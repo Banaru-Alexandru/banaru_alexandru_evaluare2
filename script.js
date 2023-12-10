b7df447076d8622da2d96bb099e5a9c9d9c3fb9c
Banaru-Alexandru <149061525+Banaru-Alexandru@users.noreply.github.com>
2023-12-10 22:31:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Cache the DOM
+var buton = document.getElementById("buton")
+var CPU = document.getElementById("CPU")
+var DISK = document.getElementById("DISK")
+var MEMORY = document.getElementById("MEMORY")
+
+function slideshow() {
+
+    DISK.src = "slideshow_placeholder.jpg";
+    CPU.src = "slideshow_placeholder.jpg";
+    MEMORY.src = "slideshow_placeholder.jpg";
+}
